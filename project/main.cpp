cdcc0f1c2007dfbdf270530773d1439aced6397e
smenon <smenon@ethz.ch>
2021-08-20 15:51:59 +0200

    added thrust input plot

--- project/main.cpp
+++ project/main.cpp
@@ -10,8 +10,13 @@ int main() {
   // Initialize visualizer
   MyApp app;
 
-  float altitude_target = 5;
-  float euler_steps = 100;
+  float altitude_target = 10;
+  float euler_steps = 1000;
+
+  // PID Gains
+  float k_p = 20;
+  float k_i = 0;
+  float k_d = 0;
 
   for (int i = 0; i < euler_steps; i++) {
     // Get system state
@@ -19,7 +24,7 @@ int main() {
 
     // Compute control input
     float altitude_error = altitude_target - quad.z_mes();
-    float thrust_input = pid(altitude_error, 20, 0, 0);
+    float thrust_input = pid(altitude_error, k_p, k_i, k_d);
 
     // Diplay the control input and error
     std::cout << "Thrust input:" << thrust_input << std::endl;
@@ -29,8 +34,9 @@ int main() {
     quad.dynamics(thrust_input, 0);
     quad.euler_step();
 
-    plot_var::xs[i] = i;
-    plot_var::ys[i] = quad.z_mes();
+    plot_var::z_plot[i] = quad.z_mes();
+    plot_var::actuator_plot[i] = thrust_input;
+    plot_var::t_plot[i] = i;
 
     std::cout << std::endl;
   }
--- project/plot.h
+++ project/plot.h
@@ -5,8 +5,17 @@ using namespace mahi::gui;
 using namespace mahi::util;
 
 namespace plot_var {
-const int euler_timesteps = 100;
-float xs[euler_timesteps], ys[euler_timesteps];
+const int euler_timesteps = 1000;
+
+float z_plot[euler_timesteps], actuator_plot[euler_timesteps],
+    t_plot[euler_timesteps];
+
+// Plot axes limits
+const int x_min = 0;
+const int x_max = euler_timesteps;
+const int y_min = 0;
+const int y_max = 20;
+
 } // namespace plot_var
 
 // Inherit from Application
@@ -23,13 +32,22 @@ public:
     static float alpha = 0.25f;
     ImGui::DragFloat("Alpha", &alpha, 0.01f, 0, 1);
     ImPlot::PushColormap(ImPlotColormap_Pastel);
-    ImPlot::SetNextPlotLimits(0, 100, 0, 50);
+    ImPlot::SetNextPlotLimits(plot_var::x_min, plot_var::x_max, plot_var::y_min,
+                              plot_var::y_max);
 
-    if (ImPlot::BeginPlot("Shaded Plots", "X-Axis", "Y-Axis")) {
-      ImPlot::PlotLine("Uncertain Data", plot_var::xs, plot_var::ys,
+    if (ImPlot::BeginPlot("Altitude vs Time", "X-Axis", "Y-Axis")) {
+      ImPlot::PlotLine("Uncertain Data", plot_var::t_plot, plot_var::z_plot,
                        plot_var::euler_timesteps);
       ImPlot::EndPlot();
     }
+
+    ImPlot::SetNextPlotLimits(plot_var::x_min, plot_var::x_max, -500, 500);
+    if (ImPlot::BeginPlot("Thrust input vs Time", "X-Axis", "Y-Axis")) {
+      ImPlot::PlotLine("Uncertain Data", plot_var::t_plot,
+                       plot_var::actuator_plot, plot_var::euler_timesteps);
+      ImPlot::EndPlot();
+    }
+
     ImGui::End();
   }
 };
