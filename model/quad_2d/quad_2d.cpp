a98b9674edfb98be07af9e255082a9bb1860796c
smenon <smenon@ethz.ch>
2021-08-31 11:17:14 +0200

    added motor dynamics, thrust conversion

--- model/quad_2d/quad_2d.cpp
+++ model/quad_2d/quad_2d.cpp
@@ -1,4 +1,23 @@
 #include "quad_2d.h"
+#include <array>
+
+void Quad2D::motor_dynamics() {
+  actual_thrust_dot = (actual_thrust - commanded_thrust) / motor_time_constant;
+}
+
+void Quad2D::motor_speed_to_thrust_map() {
+  ;
+  for (int i = 0; i < 3; i++) {
+    motor_thrusts[i] = motor_speeds[i] * k_f * k_f;
+  }
+}
+
+void Quad2D::thrust_allocation() {
+  commanded_thrust =
+      motor_speeds[0] + motor_speeds[1] + motor_speeds[2] + motor_speeds[2];
+
+  commanded_torque = motor_speeds[1] - motor_speeds[3];
+}
 
 /// Dynamics of the 2D quadcopter
 void Quad2D::dynamics(float thrust_input, float torque_input) {
@@ -19,6 +38,27 @@ void Quad2D::dynamics(float thrust_input, float torque_input) {
   beta_ddot = torque_input / inertia_2d_;
 }
 
+void Quad2D::new_dynamics(float motor_speed[4]) {
+
+  motor_speed_to_thrust_map();
+  motor_dynamics();
+
+  // // Neglect motor dynamics for now
+  // actual_thrust = thrust_input;
+
+  // x_ddot = actual_thrust * sin(beta) - drag_coeff_ * x_dot;
+
+  // if (z < 0) {
+  //   // To prevent freefall into the ground
+  //   z = 0;
+  //   z_dot = 0;
+  //   z_ddot = 0;
+  // } else
+  //   z_ddot = actual_thrust * cos(beta) - g - drag_coeff_ * fabs(z_dot);
+
+  // beta_ddot = torque_input / inertia_2d_;
+}
+
 void Quad2D::euler_step(float dt) {
 
   // Translation
@@ -31,6 +71,9 @@ void Quad2D::euler_step(float dt) {
   // Rotation
   beta = beta + beta_dot * dt;
   beta_dot = beta_dot + beta_ddot * dt;
+
+  // Motor
+  actual_thrust = actual_thrust_dot * dt + actual_thrust;
 
   // std::cout << "z_after_euler_step: " << z << std::endl;
   // std::cout << "z_dot_after_euler_step: " << z_dot << std::endl;
